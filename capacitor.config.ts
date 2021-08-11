4d470972fc776cd5517277e3bdd439ff65532242
NabilAjwad <nrosedi@novatis.com.my>
2021-08-11 11:59:03 +0800

    First Commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.crzbld.rsgquiz',
+  appName: 'RSG Quiz',
+  webDir: 'www',
+  bundledWebRuntime: false,
+};
+
+export default config;
